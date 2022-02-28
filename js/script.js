9acabb4f24bb75ce750bf7cefec22bd0eed0ccc7
robinrakibul <robinrakibulhassan@gmail.com>
2022-02-28 22:52:34 +0600

    added slice to show highest 20 data in result

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const searchPhoneModel=()=>{
     .then(res=> res.json())
     .then((data) => {
         if (data.status == true) {
-            showResult(data.data);
+            showResult(data.data.slice(0,20));
         }
         else {
           alert('No result found');
@@ -22,14 +22,14 @@ const searchPhoneModel=()=>{
 const showResult = phones => {
     const searchResult = document.getElementById('search-mobile');
     searchResult.innerHTML='';
-    phones.forEach(phone=>{
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML=`
-        <div class="container card h-100 w-50 mt-3 mb-3 rounded-3">
-            <p class="card-title font-small">${phone.phone_name}</p>
-            <img src="${phone.image}" class="card-img-top mw-100" alt="..." title="${phone.phone_name}">
-        </div>`;
-        searchResult.appendChild(div);
-    })
+        phones.forEach(phone =>{
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML=`
+            <div class="container card h-100 w-50 mt-3 mb-3 rounded-3">
+                <p class="card-title font-small">${phone.phone_name}</p>
+                <img src="${phone.image}" class="card-img-top mw-100" alt="..." title="${phone.phone_name}">
+            </div>`;
+            searchResult.appendChild(div);
+        })
 }
