b8303eb616ef6e020a5be3e852dde28c6da35d97
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-01 00:02:28 +0600

    created two new functions to show details in button

--- js/script.js
+++ js/script.js
@@ -26,11 +26,34 @@ const showResult = phones => {
         phones.forEach(phone =>{
             const div = document.createElement('div');
             div.classList.add('col');
+
             div.innerHTML=`
             <div class="container card h-100 w-50 mt-3 mb-3 rounded-3">
-                <p class="card-title font-small">${phone.phone_name}</p>
+                <p class="card-title font-small text-center">${phone.phone_name}</p>
                 <img src="${phone.image}" class="card-img-top mw-100" alt="..." title="${phone.phone_name}">
+                <button onclick="showDetails('${phone.slug}')" class="btn btn-outline-primary mt-3" type="submit">Details</button>   
             </div>`;
             searchResult.appendChild(div);
         })
+}
+
+
+// Show details while clicked
+const showDetails = phoneId =>{
+    const url=`https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayDetail(data.data))
+}
+
+const displayDetail = slug =>{
+    const phoneInfo = document.getElementById('phone-info');
+    phoneInfo.innerHTML='';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML=`
+            <div class="card-body">
+              
+            </div>`;
+    phoneInfo.appendChild(div);
 }
